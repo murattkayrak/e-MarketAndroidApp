78187ae6e9f39ba1a66c43f7b231abb7a61d0314
murattkayrak <50030357+murattkayrak@users.noreply.github.com>
2024-05-20 16:45:03 +0300

    part8
    remove unused parts

--- app/src/main/java/com/example/e_marketapp/ui/fragment/ProductFragment.kt
+++ app/src/main/java/com/example/e_marketapp/ui/fragment/ProductFragment.kt
@@ -15,20 +15,7 @@ import com.example.e_marketapp.ui.activity.MainActivity
 import com.example.e_marketapp.ui.viewmodel.HomeViewModel
 import com.example.e_marketapp.ui.viewmodel.ProductViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProductFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProductFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private var binding: FragmentProductBinding? = null
     private val productViewModel: ProductViewModel by activityViewModels()
@@ -38,8 +25,6 @@ class ProductFragment : Fragment() {
         super.onCreate(savedInstanceState)
         arguments?.let {
             product = arguments?.getParcelable<Product>("product")
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -73,25 +58,5 @@ class ProductFragment : Fragment() {
 
     private fun addToCartOnClick() {
         product?.let { (activity as MainActivity).addToCart(product = it, onClick = {}) }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProductFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProductFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
